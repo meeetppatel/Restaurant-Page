08db6bb8bef900e9a943b909ea68ea8a5247bfdd
ptlmeet27 <20124040@nuv.ac.in>
2022-07-25 23:34:01 +0530

    add images + menu page

--- main.js
+++ main.js
@@ -7,57 +7,9 @@
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "loadheader": () => (/* binding */ loadheader)
-/* harmony export */ });
-const loadheader = function () {
-  const container = document.getElementById("content");
-  container.textContent=""
-
-  const header = document.createElement("header");
-
-  const title = document.createElement("h1");
-  title.textContent = "Tuscany Courtyard";
-
-  const nav = document.createElement("div");
-  nav.classList.add("nav");
-
-  const homebtn = document.createElement("button");
-  homebtn.textContent = "Home";
-  homebtn.setAttribute("id", "homeBtn");
-  homebtn.classList.add("btns");
-
-  const menubtn = document.createElement("button");
-  menubtn.textContent = "Menu";
-  menubtn.setAttribute("id", "menuBtn");
-  menubtn.classList.add("btns");
-
-  const contactbtn = document.createElement("button");
-  contactbtn.textContent = "Contact";
-  contactbtn.setAttribute("id", "contactBtn");
-  contactbtn.classList.add("btns");
-
-  nav.appendChild(homebtn);
-  nav.appendChild(menubtn);
-  nav.appendChild(contactbtn);
-
-  header.appendChild(title);
-  header.appendChild(nav);
-
-  container.appendChild(header);
-};
-
-
-
-
-/***/ }),
-/* 2 */
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "loadhome": () => (/* binding */ loadhome)
 /* harmony export */ });
-/* harmony import */ var _header__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
+/* harmony import */ var _header__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
 /* harmony import */ var _footer__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(3);
 
 
@@ -110,6 +62,54 @@ const loadhome = function (){
 
 
 /***/ }),
+/* 2 */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "loadheader": () => (/* binding */ loadheader)
+/* harmony export */ });
+const loadheader = function () {
+  const container = document.getElementById("content");
+  container.textContent=""
+
+  const header = document.createElement("header");
+
+  const title = document.createElement("h1");
+  title.textContent = "Tuscany Courtyard";
+
+  const nav = document.createElement("div");
+  nav.classList.add("nav");
+
+  const homebtn = document.createElement("button");
+  homebtn.textContent = "Home";
+  homebtn.setAttribute("id", "homeBtn");
+  homebtn.classList.add("btns");
+
+  const menubtn = document.createElement("button");
+  menubtn.textContent = "Menu";
+  menubtn.setAttribute("id", "menuBtn");
+  menubtn.classList.add("btns");
+
+  const contactbtn = document.createElement("button");
+  contactbtn.textContent = "Contact";
+  contactbtn.setAttribute("id", "contactBtn");
+  contactbtn.classList.add("btns");
+
+  nav.appendChild(homebtn);
+  nav.appendChild(menubtn);
+  nav.appendChild(contactbtn);
+
+  header.appendChild(title);
+  header.appendChild(nav);
+
+  container.appendChild(header);
+};
+
+
+
+
+/***/ }),
 /* 3 */
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
@@ -138,7 +138,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "loadmenu": () => (/* binding */ loadmenu)
 /* harmony export */ });
-/* harmony import */ var _header__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
+/* harmony import */ var _header__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
 /* harmony import */ var _footer__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(3);
 
 
@@ -152,11 +152,52 @@ const loadmenu = function() {
     const main = document.createElement("div");
     main.classList.add("main");
 
+    const menu = document.createElement("div");
+    menu.classList.add("menu");
 
+    const item1 = createitem("pizza1","asdadadadaasda", "./images/pizza1.jpg");
+    const item2 = createitem("pizza2","asdadadadaasda", "./images/pizza2.jpg");
+    const item3 = createitem("pizza3","asdadadadaasda", "./images/pizza3.jpg");
+    const item4 = createitem("pizza4","asdadadadaasda", "./images/pizza4.jpg");
+    const item5 = createitem("pizza5","asdadadadaasda", "./images/pizza5.jpg");
+    const item6 = createitem("pizza6","asdadadadaasda", "./images/pizza6.jpg");
+    const item7 = createitem("pizza7","asdadadadaasda", "./images/pizza7.jpg");
+    const item8 = createitem("pizza8","asdadadadaasda", "./images/pizza8.jpg");
 
+    menu.appendChild(item1);
+    menu.appendChild(item2);
+    menu.appendChild(item3);
+    menu.appendChild(item4);
+    menu.appendChild(item5);
+    menu.appendChild(item6);
+    menu.appendChild(item7);
+    menu.appendChild(item8);
 
+    main.appendChild(menu);
+    container.appendChild(main);
 
     (0,_footer__WEBPACK_IMPORTED_MODULE_1__.loadfooter)();
+}
+
+function createitem(name, info ,img){
+    const item = document.createElement("div");
+    item.classList.add("items");
+
+    const itemimg = document.createElement("img");
+    itemimg.src = `${img}`;
+
+    const itemname = document.createElement("h3");
+    itemname.textContent = `${name}`;
+
+    const details = document.createElement("p");
+    details.textContent=`${info}`;
+
+
+    item.appendChild(itemimg);
+    item.appendChild(itemname);
+    item.appendChild(details);
+
+    return item;
 }
 
 
@@ -169,7 +210,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "loadcontact": () => (/* binding */ loadcontact)
 /* harmony export */ });
-/* harmony import */ var _header__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
+/* harmony import */ var _header__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
 /* harmony import */ var _footer__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(3);
 
 
@@ -276,36 +317,29 @@ var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _header__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
-/* harmony import */ var _home__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(2);
-/* harmony import */ var _menu__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(4);
-/* harmony import */ var _contact__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(5);
-/* harmony import */ var _footer__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(3);
+/* harmony import */ var _home__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
+/* harmony import */ var _menu__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(4);
+/* harmony import */ var _contact__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(5);
 
 
 
 
-
-
-
-(0,_home__WEBPACK_IMPORTED_MODULE_1__.loadhome)();
+(0,_home__WEBPACK_IMPORTED_MODULE_0__.loadhome)();
 
 const tabswich = function(){
     document.getElementById("homeBtn").addEventListener("click", function(){
-        (0,_home__WEBPACK_IMPORTED_MODULE_1__.loadhome)();
+        (0,_home__WEBPACK_IMPORTED_MODULE_0__.loadhome)();
         tabswich();
     });
     document.getElementById("contactBtn").addEventListener("click",function(){
-        (0,_contact__WEBPACK_IMPORTED_MODULE_3__.loadcontact)();      
+        (0,_contact__WEBPACK_IMPORTED_MODULE_2__.loadcontact)();      
         tabswich();
     });
     document.getElementById("menuBtn").addEventListener("click",function(){
-        (0,_menu__WEBPACK_IMPORTED_MODULE_2__.loadmenu)();
+        (0,_menu__WEBPACK_IMPORTED_MODULE_1__.loadmenu)();
         tabswich();
     })
-    
 };
-
 tabswich(); 
 })();
 
